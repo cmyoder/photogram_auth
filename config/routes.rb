996e2bfde3ae0894019e5668f227f1e2d3771485
cmyoder <cmyoder@uchicago.edu>
2017-02-28 19:32:47 -0600

    RCAV for users

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
 end
